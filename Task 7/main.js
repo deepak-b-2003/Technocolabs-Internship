f45b6b3620e79c981be1e7621d25b07a5cea667c
deepak-b-2003 <bijivemuladeepakreddy@gmail.com>
2023-05-22 01:31:26 +0530

    uploading tasks

--- Task 7/main.js
+++ Task 7/main.js
@@ -9,6 +9,6 @@ laterYears = laterYears * 4; //each later years equates to 4 dog years
 const myAgeInDogYears = earlyYears + laterYears; //finding my age in dog years
 
 //storing my name and printing my age in dog years
-let myName = "Pradeep";
+let myName = "Deepak";
 myName = myName.toLowerCase(); // converting all characters to lower case in myName
 console.log(`\nMy name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.\n`);
